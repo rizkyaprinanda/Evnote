968aa65c578b9becc08a7153000e93abf99c33e9
Rizky Aprinanda <rizkyaprinanda@gmail.com>
2023-12-08 06:41:48 +0700

    Memperbaiki permasalahan onBackPressed, menambahkan fitur pencarian pada TopRatedFragment

--- app/src/main/java/com/example/evnote/view/TopRatedFragment.kt
+++ app/src/main/java/com/example/evnote/view/TopRatedFragment.kt
@@ -1,15 +1,17 @@
 package com.example.evnote.view
 
-import android.content.Intent
 import android.content.res.ColorStateList
-import android.graphics.PorterDuff
-import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -23,12 +25,23 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+// Fungsi ekstensi untuk melakukan pencarian pada List<Movie>
+private fun List<Movie>.performSearch(query: String): List<Movie> {
+    return this.filter {
+        it.title?.contains(query, ignoreCase = true) == true ||
+                it.overview?.contains(query, ignoreCase = true) == true
+    }
+}
+
 class TopRatedFragment : Fragment() {
     private lateinit var binding: FragmentTopRatedBinding
     private lateinit var adapter1: MovieAdapter
     private lateinit var adapter2: MovieAdapter
     private lateinit var progressBar: ProgressBar
-    private lateinit var progressBar2 : ProgressBar
+    private lateinit var progressBar2: ProgressBar
+    private var doubleBackPressedOnce = false
+    private var originalData: List<Movie> = emptyList()
+    private var topMovies: List<Movie> = emptyList()
 
     companion object {
         fun newInstance(): TopRatedFragment {
@@ -39,7 +52,7 @@ class TopRatedFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTopRatedBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -48,32 +61,30 @@ class TopRatedFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                val intent = Intent(requireContext(), HomeActivity::class.java)
-                startActivity(intent)
-                requireActivity().finish()
 
+            override fun handleOnBackPressed() {
+                if (!doubleBackPressedOnce) {
+                    Toast.makeText(requireContext(), "Press again to exit", Toast.LENGTH_SHORT).show()
+                    doubleBackPressedOnce = true
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        doubleBackPressedOnce = false
+                    }, 2000)
+                } else {
+                    requireActivity().finish()
+                }
             }
         })
 
         adapter1 = MovieAdapter(requireContext(), arrayListOf())
         adapter2 = MovieAdapter(requireContext(), arrayListOf())
 
-        // Inisialisasi ProgressBar
         progressBar = binding.progressBar
         progressBar2 = binding.progressBar2
 
-        // Ganti dengan warna yang diinginkan
         val color = ContextCompat.getColor(requireContext(), R.color.yellow)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            progressBar.indeterminateTintList = ColorStateList.valueOf(color)
-            progressBar2.indeterminateTintList = ColorStateList.valueOf(color)
-        } else {
-            val mode = PorterDuff.Mode.SRC_IN
-            progressBar.indeterminateDrawable.setColorFilter(color, mode)
-            progressBar2.indeterminateDrawable.setColorFilter(color, mode)
-        }
+        progressBar.indeterminateTintList = ColorStateList.valueOf(color)
+        progressBar2.indeterminateTintList = ColorStateList.valueOf(color)
 
         binding.rvMovie.adapter = adapter1
         binding.rvMovie.setHasFixedSize(true)
@@ -81,59 +92,110 @@ class TopRatedFragment : Fragment() {
         binding.rvMovie2.adapter = adapter2
         binding.rvMovie2.setHasFixedSize(true)
 
-        // Show progress bar before making network requests
         progressBar.visibility = View.VISIBLE
         progressBar2.visibility = View.VISIBLE
 
-        remoteGetTopRatedMovies(adapter1, binding.progressBar)
-        remoteGetHeroMovies(adapter2, binding.progressBar2)
+        remoteGetTopMovies(adapter1, progressBar) { movies ->
+            topMovies = movies
+            performSearch(binding.edtSearch.text.toString())
+        }
+
+        remoteGetHeroMovies(adapter2, progressBar2)
+
+        val search = binding.edtSearch
+        val noResultTextView = binding.tvNoResult
+        noResultTextView.visibility = View.GONE
+
+        search.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int) {
+                val query = charSequence.toString()
+                performSearch(query)
+            }
+
+            override fun afterTextChanged(editable: Editable) {}
+        })
+
+        // Simpan data asli untuk pencarian
+        originalData = ArrayList()
     }
 
-    private fun remoteGetTopRatedMovies(adapter: MovieAdapter, progressBar: ProgressBar) {
-        MovieClient.movieService.getTopRatedMovies().enqueue(object : Callback<Movies> {
+    private fun performSearch(query: String) {
+        val filteredData = topMovies.performSearch(query)
+        val searchText = binding.edtSearch
+
+        if (filteredData.isEmpty()) {
+            binding.tvNoResult.visibility = View.VISIBLE
+            adapter1.setData(emptyList())
+            binding.rvMovie.visibility = View.GONE
+            binding.rvMovie2.visibility = View.GONE
+
+            if (searchText.text.isEmpty()) {
+                binding.tvTop.visibility = View.VISIBLE
+                binding.tvOnTrend.visibility = View.VISIBLE
+                adapter1.setData(emptyList())
+            }
+
+        } else {
+            binding.tvNoResult.visibility = View.GONE
+            binding.tvTop.visibility = View.GONE
+            binding.tvOnTrend.visibility = View.GONE
+            binding.rvMovie.visibility = View.VISIBLE
+            binding.rvMovie2.visibility = View.GONE
+            adapter1.setData(filteredData)
+
+            if (searchText.text.isEmpty()) {
+                binding.tvTop.visibility = View.VISIBLE
+                binding.tvOnTrend.visibility = View.VISIBLE
+                binding.rvMovie2.visibility = View.VISIBLE
+                adapter1.setData(filteredData)
+            }
+        }
+    }
+
+    private fun remoteGetMovies(
+        call: Call<Movies>,
+        adapter: MovieAdapter,
+        progressBar: ProgressBar,
+        callback: (List<Movie>) -> Unit
+    ) {
+        call.enqueue(object : Callback<Movies> {
             override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
-                // Hide progress bar on response
                 progressBar.visibility = View.GONE
 
                 if (response.isSuccessful) {
                     val data = response.body()?.results
                     Log.d("MovieResponse", data.toString())
+
+                    // Shuffle data sebelum menetapkan ke adapter
                     data?.let { setDataToAdapter(adapter, it) }
+                    callback.invoke(data ?: emptyList())
                 }
             }
 
             override fun onFailure(call: Call<Movies>, t: Throwable) {
-                // Hide progress bar on failure
                 progressBar.visibility = View.GONE
-
                 Log.d("Error", "" + t.stackTraceToString())
             }
         })
+    }
+
+    private fun remoteGetTopMovies(adapter: MovieAdapter, progressBar: ProgressBar, callback: (List<Movie>) -> Unit) {
+        val call = MovieClient.movieService.getTopRatedMovies()
+        remoteGetMovies(call, adapter, progressBar) { movies ->
+            // Melakukan callback ke fungsi yang mengonsumsinya
+            callback.invoke(movies)
+        }
     }
 
     private fun remoteGetHeroMovies(adapter: MovieAdapter, progressBar: ProgressBar) {
-        MovieClient.movieService.getHeroMovies().enqueue(object : Callback<Movies> {
-            override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
-                // Hide progress bar on response
-                progressBar2.visibility = View.GONE
-
-                if (response.isSuccessful) {
-                    val data = response.body()?.results
-                    Log.d("MovieResponse", data.toString())
-                    data?.let { setDataToAdapter(adapter, it) }
-                }
-            }
-
-            override fun onFailure(call: Call<Movies>, t: Throwable) {
-                // Hide progress bar on response
-                progressBar2.visibility = View.GONE
-
-                Log.d("Error", "" + t.stackTraceToString())
-            }
-        })
+        val call = MovieClient.movieService.getHeroMovies()
+        remoteGetMovies(call, adapter, progressBar) {}
     }
 
     private fun setDataToAdapter(adapter: MovieAdapter, data: List<Movie>) {
+        originalData = data
         adapter.setData(data)
     }
 }
